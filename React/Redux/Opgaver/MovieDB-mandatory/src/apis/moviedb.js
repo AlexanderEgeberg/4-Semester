f06d7e7300e8eb36a678202dfd09a81d222c2bc7
Alexander Egeberg <alexanderegeberg@hotmail.com>
2021-04-20 20:45:06 +0200

    Rykkede mandatory opgave til eget repo

--- React/Redux/Opgaver/MovieDB-mandatory/src/apis/moviedb.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-
-
-export default axios.create({baseURL:'http://api.themoviedb.org/3/search/movie?query='});
--- React/Redux/Opgaver/MovieDB-mandatory/src/reducers/movieReducer.js
+++
@@ -1,6 +0,0 @@
-export default (state=[], action) => {
-    switch (action.type) {
-        case 'FETCH_MOVIES': return action.payload;
-        default: return state; 
-    }
- };
