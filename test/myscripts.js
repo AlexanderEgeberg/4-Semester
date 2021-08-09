d6f145afd168d5109d52682d44e01e0617d62501
Alexander Egeberg <alexanderegeberg@hotmail.com>
2021-08-09 07:43:39 +0200

    update

---
+++ test/myscripts.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    axios.get('https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json')
+    .then(function(response){
+        console.log(response.data);
+    }); 
+    console.log("test");
+};
